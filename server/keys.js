e0ff2eef175fabdb8c9f55df06f5e175ffd8b8e5
thanhhv317 <thanhhoang317@gmail.com>
2023-04-09 16:12:43 +0700

    Initial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABSE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
+};
